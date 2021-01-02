1e631ce3b6a186b3a62b0db481975bcba68e2037
Ahmed Muyen Kamran <akamran@colgate.edu>
2021-01-01 21:28:05 -0500

    changed code layout

---
+++ app/db/mockData.js
@@ -0,0 +1,82 @@
+const mockData = [
+    {   key:1,
+        class:"Sociology",
+        details:"In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat.",
+        type:"nec condimentum",
+        due_date:"03/25/2020",
+        priority:"medium"
+    },
+    {
+        key:2,
+        class:"Calculus",
+        details:"Morbi porttitor lorem key ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat.",
+        type:"placerat ante",
+        due_date:"03/30/2020",
+        priority:"high"
+    },
+    {
+        key:3,
+        class:"Calculus",
+        details:"Phasellus in felis. Donec semper sapien a libero.",
+        type:"lectus",
+        due_date:"12/12/2020",
+        priority:"medium"
+    },
+    {
+        key:4,
+        class:"Computer Science",
+        details:"Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
+        type:"volutpat quam",
+        due_date:"12/28/2020",
+        priority:"high"
+    },
+    {
+        key:5,
+        class:"Calculus",
+        details:"Morbi porttitor lorem key ligula. Suspendisse ornare consequat lectus.",
+        type:"ut tellus",
+        due_date:"06/01/2020",
+        priority:"low"
+    },
+    {
+        key:6,
+        class:"Computer Science",
+        details:"Proin interdum mauris non ligula pellentesque ultrices. Phasellus key sapien in sapien iaculis congue.",
+        type:"nec",
+        due_date:"05/31/2020",
+        priority:"high"
+    },
+    {
+        key:7,
+        class:"Sociology",
+        details:"In congue. Etiam justo. Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna.",
+        type:"dui",
+        due_date:"04/21/2020",
+        priority:"low"},
+    {
+        key:8,
+        class:"Sociology",
+        details:"Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.",
+        type:"morbi",
+        due_date:"02/02/2020",
+        priority:"low"
+    },
+    {
+        key:9,
+        class:"Computer Science",
+        details:"Vivamus tortor. Duis mattis egestas metus.",
+        type:"volutpat",
+        due_date:"08/02/2020",
+        priority:"medium"
+    },
+    {
+        key:10,
+        class:"Computer Science",
+        details:"Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor.",
+        type:"in",
+        due_date:"09/29/2020",
+        priority:"medium"
+    }
+]
+
+export default mockData;
--- app/screens/AssignmentDetailScreen.js
+++ app/screens/AssignmentDetailScreen.js
@@ -3,67 +3,68 @@ import { StyleSheet, Text, View, SafeAreaView, Button, StatusBar, FlatList } fro
 
 function AssignmentDetailScreen({navigation}){
     return (
-        <SafeAreaView style={{flex:1, backgroundColor: "#ffffff"}}>
-            <View style={{
-            borderColor: "black",
-            borderWidth:1,
-            backgroundColor: "#ffb3ba",
-            padding: 20,
-            marginVertical: 8,
-            marginHorizontal: 16,
-          }}>
-            <Text style = {{
-                    fontSize: 20,
-                    fontWeight: "bold",
-                    color: "black"
-            }}>
-                {navigation.getParam('class')}
+      <SafeAreaView style={styles.container}>
+          <View style = {styles.class}>
+          <Text style = {styles.text}>
+            {navigation.getParam('class')}
+          </Text>
+        </View>
+        <View style={styles.description}>
+          <Text style = {styles.text}>
+            <Text
+                  style = {{
+                      textDecorationLine: 'underline'
+                  }}
+              >{navigation.getParam('type')} {"\n \n"}
             </Text>
-          </View>
+            {navigation.getParam('details')}
+          </Text>
+        </View>
 
-          <View style={{
-            flex:1,
-            borderColor: "black",
-            borderWidth:1,
-            backgroundColor: "#ffdfba",
-            padding: 20,
-            marginVertical: 8,
-            marginHorizontal: 16,
-          }}>
-            <Text style = {{
-                    fontSize: 20,
-                    fontWeight: "bold",
-                    color: "black"
-            }}>
-                <Text
-                    style = {{
-                        textDecorationLine: 'underline'
-                    }}
-                >{navigation.getParam('type')} {"\n \n"}</Text>
-
-                {navigation.getParam('details')}
-            </Text>
-          </View>
-
-          <View style={{
-            borderColor: "black",
-            borderWidth:1,
-            backgroundColor: "#bae1ff",
-            padding: 20,
-            marginVertical: 8,
-            marginHorizontal: 16,
-          }}>
-            <Text style = {{
-                    fontSize: 20,
-                    fontWeight: "bold",
-                    color: "black"
-            }}>
-                {navigation.getParam('due_date')}
-            </Text>
-          </View>
-        </SafeAreaView>
-    );
+        <View style={styles.due_date}>
+          <Text style = {styles.text}>
+            {navigation.getParam('due_date')}
+          </Text>
+        </View>
+      </SafeAreaView>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex:1, 
+    backgroundColor: "#ffffff"
+  },
+  text : {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "black"
+  },
+  class: {
+    borderColor: "black",
+    borderWidth:1,
+    backgroundColor: "#ffb3ba",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  description:{
+    flex:1,
+    borderColor: "black",
+    borderWidth:1,
+    backgroundColor: "#ffdfba",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16
+  },
+  due_date:{
+    borderColor: "black",
+    borderWidth:1,
+    backgroundColor: "#bae1ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  }
+})
 
 export default AssignmentDetailScreen;
